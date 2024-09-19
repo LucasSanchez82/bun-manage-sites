767d9d753ee263f61bb0a7c912da3703e3ff09a9
Lucas <jiraya1008@gmail.com>
2024-09-19 17:09:54 +0200

    feature : endpoint for teste nginx conf file

--- src/nginx/conf/index.ts
+++ src/nginx/conf/index.ts
@@ -26,9 +26,21 @@ const enabled: RequestHandler = async (req, res) => {
 const all: RequestHandler = async (req, res) => {
     const { stdout: stdoutAvailable, stderr: stderrAvailable } = await getAvailable();
     const { stdout: stdoutEnabled, stderr: stderrEnabled } = await getEnabled();
-    res.json({ domains: {available: stdoutAvailable.trim().split('\n'), enabled: stdoutEnabled.trim().split('\n')}, error: stderrAvailable || stderrEnabled || undefined });
+    res.json({ domains: { available: stdoutAvailable.trim().split('\n'), enabled: stdoutEnabled.trim().split('\n') }, error: stderrAvailable || stderrEnabled || undefined });
 }
 
-const getNginxConf = { available, enabled, all } 
+const status: RequestHandler = async (req, res) => {
+    const formatStrToArray = (str: string) =>
+        str
+            .replaceAll('nginx: configuration file', '')
+            .replaceAll('nginx: the configuration file', '')
+            .replaceAll('\t', ' ')
+            .trim()
+            .split('\n')
+            .map(line => line.trim());
+    const { stdout, stderr } = getStdByOutput(await $`echo ${process.env.SUDO_PASSWORD} | sudo -S nginx -t`);
+    res.json({ status: formatStrToArray(stdout), error: formatStrToArray(stderr)  || undefined });
+}
+const getNginxConf = { available, enabled, all, status }
 
 export default getNginxConf;
